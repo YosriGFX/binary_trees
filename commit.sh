4e8028575668dc0e688f39f441825f38c31e5bdb
YosriGFX <YosriG1997@gmail.com>
2020-03-13 14:57:14 +0100

    hey

--- commit.sh
+++ commit.sh
@@ -2,7 +2,34 @@
 #Commiter
 
 chmod 777 *
-git add --all
+
+git add 0-binary_tree_node.c
+git add 1-binary_tree_insert_left.c
+git add 10-binary_tree_depth.c
+git add 100-binary_trees_ancestor.c
+git add 101-binary_tree_levelorder.c
+git add 103-binary_tree_rotate_left.c
+git add 104-binary_tree_rotate_right.c
+git add 11-binary_tree_size.c
+git add 12-binary_tree_leaves.c
+git add 13-binary_tree_nodes.c
+git add 14-binary_tree_balance.c
+git add 15-binary_tree_is_full.c
+git add 16-binary_tree_is_perfect.c
+git add 17-binary_tree_sibling.c
+git add 18-binary_tree_uncle.c
+git add 2-binary_tree_insert_right.c
+git add 3-binary_tree_delete.c
+git add 4-binary_tree_is_leaf.c
+git add 5-binary_tree_is_root.c
+git add 6-binary_tree_preorder.c
+git add 7-binary_tree_inorder.c
+git add 8-binary_tree_postorder.c
+git add 9-binary_tree_height.c
+git add readme.md
+git add binary_tree_print.c
+git add binary_trees.h
+
 git commit -m "[YOSRI- 0-binary_tree_node.c]"
 git commit -m "[YOSRI- 1-binary_tree_insert_left.c]"
 git commit -m "[YOSRI- 10-binary_tree_depth.c]"
@@ -32,7 +59,34 @@ git commit -m "[YOSRI- binary_trees.h]"
 git push
 clear
 chmod 400 *
-git add --all
+
+git add 0-binary_tree_node.c
+git add 1-binary_tree_insert_left.c
+git add 10-binary_tree_depth.c
+git add 100-binary_trees_ancestor.c
+git add 101-binary_tree_levelorder.c
+git add 103-binary_tree_rotate_left.c
+git add 104-binary_tree_rotate_right.c
+git add 11-binary_tree_size.c
+git add 12-binary_tree_leaves.c
+git add 13-binary_tree_nodes.c
+git add 14-binary_tree_balance.c
+git add 15-binary_tree_is_full.c
+git add 16-binary_tree_is_perfect.c
+git add 17-binary_tree_sibling.c
+git add 18-binary_tree_uncle.c
+git add 2-binary_tree_insert_right.c
+git add 3-binary_tree_delete.c
+git add 4-binary_tree_is_leaf.c
+git add 5-binary_tree_is_root.c
+git add 6-binary_tree_preorder.c
+git add 7-binary_tree_inorder.c
+git add 8-binary_tree_postorder.c
+git add 9-binary_tree_height.c
+git add readme.md
+git add binary_tree_print.c
+git add binary_trees.h
+
 git commit -m "[YOSRI- 0-binary_tree_node.c]"
 git commit -m "[YOSRI- 1-binary_tree_insert_left.c]"
 git commit -m "[YOSRI- 10-binary_tree_depth.c]"
@@ -62,7 +116,34 @@ git commit -m "[YOSRI- binary_trees.h]"
 git push
 clear
 chmod 777 *
-git add --all
+
+git add 0-binary_tree_node.c
+git add 1-binary_tree_insert_left.c
+git add 10-binary_tree_depth.c
+git add 100-binary_trees_ancestor.c
+git add 101-binary_tree_levelorder.c
+git add 103-binary_tree_rotate_left.c
+git add 104-binary_tree_rotate_right.c
+git add 11-binary_tree_size.c
+git add 12-binary_tree_leaves.c
+git add 13-binary_tree_nodes.c
+git add 14-binary_tree_balance.c
+git add 15-binary_tree_is_full.c
+git add 16-binary_tree_is_perfect.c
+git add 17-binary_tree_sibling.c
+git add 18-binary_tree_uncle.c
+git add 2-binary_tree_insert_right.c
+git add 3-binary_tree_delete.c
+git add 4-binary_tree_is_leaf.c
+git add 5-binary_tree_is_root.c
+git add 6-binary_tree_preorder.c
+git add 7-binary_tree_inorder.c
+git add 8-binary_tree_postorder.c
+git add 9-binary_tree_height.c
+git add readme.md
+git add binary_tree_print.c
+git add binary_trees.h
+
 git commit -m "[YOSRI- 0-binary_tree_node.c]"
 git commit -m "[YOSRI- 1-binary_tree_insert_left.c]"
 git commit -m "[YOSRI- 10-binary_tree_depth.c]"
@@ -92,7 +173,34 @@ git commit -m "[YOSRI- binary_trees.h]"
 git push
 clear
 chmod 400 *
-git add --all
+
+git add 0-binary_tree_node.c
+git add 1-binary_tree_insert_left.c
+git add 10-binary_tree_depth.c
+git add 100-binary_trees_ancestor.c
+git add 101-binary_tree_levelorder.c
+git add 103-binary_tree_rotate_left.c
+git add 104-binary_tree_rotate_right.c
+git add 11-binary_tree_size.c
+git add 12-binary_tree_leaves.c
+git add 13-binary_tree_nodes.c
+git add 14-binary_tree_balance.c
+git add 15-binary_tree_is_full.c
+git add 16-binary_tree_is_perfect.c
+git add 17-binary_tree_sibling.c
+git add 18-binary_tree_uncle.c
+git add 2-binary_tree_insert_right.c
+git add 3-binary_tree_delete.c
+git add 4-binary_tree_is_leaf.c
+git add 5-binary_tree_is_root.c
+git add 6-binary_tree_preorder.c
+git add 7-binary_tree_inorder.c
+git add 8-binary_tree_postorder.c
+git add 9-binary_tree_height.c
+git add readme.md
+git add binary_tree_print.c
+git add binary_trees.h
+
 git commit -m "[YOSRI- 0-binary_tree_node.c]"
 git commit -m "[YOSRI- 1-binary_tree_insert_left.c]"
 git commit -m "[YOSRI- 10-binary_tree_depth.c]"
@@ -122,7 +230,34 @@ git commit -m "[YOSRI- binary_trees.h]"
 git push
 clear
 chmod 777 *
-git add --all
+
+git add 0-binary_tree_node.c
+git add 1-binary_tree_insert_left.c
+git add 10-binary_tree_depth.c
+git add 100-binary_trees_ancestor.c
+git add 101-binary_tree_levelorder.c
+git add 103-binary_tree_rotate_left.c
+git add 104-binary_tree_rotate_right.c
+git add 11-binary_tree_size.c
+git add 12-binary_tree_leaves.c
+git add 13-binary_tree_nodes.c
+git add 14-binary_tree_balance.c
+git add 15-binary_tree_is_full.c
+git add 16-binary_tree_is_perfect.c
+git add 17-binary_tree_sibling.c
+git add 18-binary_tree_uncle.c
+git add 2-binary_tree_insert_right.c
+git add 3-binary_tree_delete.c
+git add 4-binary_tree_is_leaf.c
+git add 5-binary_tree_is_root.c
+git add 6-binary_tree_preorder.c
+git add 7-binary_tree_inorder.c
+git add 8-binary_tree_postorder.c
+git add 9-binary_tree_height.c
+git add readme.md
+git add binary_tree_print.c
+git add binary_trees.h
+
 git commit -m "[YOSRI- 0-binary_tree_node.c]"
 git commit -m "[YOSRI- 1-binary_tree_insert_left.c]"
 git commit -m "[YOSRI- 10-binary_tree_depth.c]"
@@ -152,7 +287,34 @@ git commit -m "[YOSRI- binary_trees.h]"
 git push
 clear
 chmod 400 *
-git add --all
+
+git add 0-binary_tree_node.c
+git add 1-binary_tree_insert_left.c
+git add 10-binary_tree_depth.c
+git add 100-binary_trees_ancestor.c
+git add 101-binary_tree_levelorder.c
+git add 103-binary_tree_rotate_left.c
+git add 104-binary_tree_rotate_right.c
+git add 11-binary_tree_size.c
+git add 12-binary_tree_leaves.c
+git add 13-binary_tree_nodes.c
+git add 14-binary_tree_balance.c
+git add 15-binary_tree_is_full.c
+git add 16-binary_tree_is_perfect.c
+git add 17-binary_tree_sibling.c
+git add 18-binary_tree_uncle.c
+git add 2-binary_tree_insert_right.c
+git add 3-binary_tree_delete.c
+git add 4-binary_tree_is_leaf.c
+git add 5-binary_tree_is_root.c
+git add 6-binary_tree_preorder.c
+git add 7-binary_tree_inorder.c
+git add 8-binary_tree_postorder.c
+git add 9-binary_tree_height.c
+git add readme.md
+git add binary_tree_print.c
+git add binary_trees.h
+
 git commit -m "[YOSRI- 0-binary_tree_node.c]"
 git commit -m "[YOSRI- 1-binary_tree_insert_left.c]"
 git commit -m "[YOSRI- 10-binary_tree_depth.c]"
@@ -182,7 +344,34 @@ git commit -m "[YOSRI- binary_trees.h]"
 git push
 clear
 chmod 777 *
-git add --all
+
+git add 0-binary_tree_node.c
+git add 1-binary_tree_insert_left.c
+git add 10-binary_tree_depth.c
+git add 100-binary_trees_ancestor.c
+git add 101-binary_tree_levelorder.c
+git add 103-binary_tree_rotate_left.c
+git add 104-binary_tree_rotate_right.c
+git add 11-binary_tree_size.c
+git add 12-binary_tree_leaves.c
+git add 13-binary_tree_nodes.c
+git add 14-binary_tree_balance.c
+git add 15-binary_tree_is_full.c
+git add 16-binary_tree_is_perfect.c
+git add 17-binary_tree_sibling.c
+git add 18-binary_tree_uncle.c
+git add 2-binary_tree_insert_right.c
+git add 3-binary_tree_delete.c
+git add 4-binary_tree_is_leaf.c
+git add 5-binary_tree_is_root.c
+git add 6-binary_tree_preorder.c
+git add 7-binary_tree_inorder.c
+git add 8-binary_tree_postorder.c
+git add 9-binary_tree_height.c
+git add readme.md
+git add binary_tree_print.c
+git add binary_trees.h
+
 git commit -m "[YOSRI- 0-binary_tree_node.c]"
 git commit -m "[YOSRI- 1-binary_tree_insert_left.c]"
 git commit -m "[YOSRI- 10-binary_tree_depth.c]"
